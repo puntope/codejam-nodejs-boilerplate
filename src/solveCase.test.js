e1579ec8106c3f281bf34f0718d68897664b8ce9
miguel <miguel@buidinamsterdam.com>
2020-04-03 08:16:11 +0200

    Cleaner tests

--- src/solveCase.test.js
+++ src/solveCase.test.js
@@ -1,13 +1,13 @@
 import solveCase from "./solveCase";
 
-test('4 is equal to 1 + 3', () => {
-    expect(solveCase([4])).toBe('1 3');
-});
+const tests = [
+   [[4],'1 3'],
+   [[940],'910 30'],
+   [[4444],'1111 3333'],
+];
 
-test('940 is equal to 910 + 30', () => {
-    expect(solveCase([940])).toBe('910 30');
-});
-
-test('4444 is equal to 1111 + 3333', () => {
-    expect(solveCase([4444])).toBe('1111 3333');
-});
+for (let i = 0; i < tests.length; i++) {
+    test(`${tests[i][0].join(' ')} is equal to ${tests[i][1]}`, () => {
+        expect(solveCase(tests[i][0])).toBe(tests[i][1]);
+    });
+}
